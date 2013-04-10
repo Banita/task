5a75235d3fc98e7f07b5b4ee143eb3f84bfe7f51
Banita Patro <banita.patro@strata.co.in>
2013-04-10 17:39:36 +0530

    generate code change

--- app/models/secret_code.rb
+++ app/models/secret_code.rb
@@ -2,11 +2,10 @@ class SecretCode < ActiveRecord::Base
   # attr_accessible :title, :body
   has_one :user
     def self.generate_codes(number)
-  	  a=(1..number).to_a.shuffle
-  	  a.each do |i|
-  		 secret_code = SecretCode.new
-  		 secret_code.secret_code=i
-  		 secret_code.save
+    	number.times do 
+    		secret_code = SecretCode.new
+            secret_code.secret_code=rand(1000)
+            secret_code.save
   	   end	
    end
 end
